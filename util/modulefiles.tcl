824b4bb26d7af64a281ffdc7d32006514b244edf
Nathan Baltzell <baltzell@gmx.com>
2024-05-16 13:27:07 -0400

    Revert "remove duplication"
    This reverts commit 4e6e650ccf1f8fd6f2e7f9e0ad77768b6fd7ec62.

--- util/modulefiles.tcl
+++ util/modulefiles.tcl
@@ -45,7 +45,11 @@ proc home {} {
 # Get the $OSRELEASE environment variable if it exists, else
 # run our script to get what it will become upon loading modules:
 proc osrelease {} {
-    return [ getenv OSRELEASE [exec [home]/util/osrelease.py] ]
+    global env
+    if { [ info exists env(OSRELEASE) ] } {
+        return $env(OSRELEASE)
+    }
+    return [ exec [home]/util/osrelease.py ]
 }
 
 # list all site-specific procedures to expose to modulefile and modulerc
